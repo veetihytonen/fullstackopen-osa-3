27e21e5f401d2c633b9632252c4118e694cbfd8a
Veeti Hytönen <veeti.hytonen@gmail.com>
2023-12-09 13:16:56 +0200

    did exercises up to 3.2

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}<p/>
+    </div>
+  `)
+})
+
+app.get('/api/person', (req, res) => {
+  res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
